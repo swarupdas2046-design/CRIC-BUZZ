0fcc4d9d52e6c4d87a4248439323d4e086793e65
Swarup Das <swarupdas2046@gmail.com>
2026-06-14 17:55:06 +0530

    Implement private series backend module

---
+++ Server/src/modules/private/series/interfaces/series.interface.js
@@ -0,0 +1,10 @@
+export const SeriesInterface = {
+  name: "string",
+  shortName: "string",
+  season: "string",
+  status: "string",
+  logo: "string",
+  isDeleted: "boolean",
+  createdBy: "ObjectId",
+  updatedBy: "ObjectId",
+};
