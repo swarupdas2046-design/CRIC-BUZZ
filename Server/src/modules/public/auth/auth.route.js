af6ec000f39e47125dbddaee8b9ed3c98b75f229
Avesh Maurya <aveshmaurya9453@gmail.com>
2026-06-14 14:52:16 +0530

    feat: add async handler utility for controller error handling

--- Server/src/modules/public/auth/auth.route.js
+++ Server/src/modules/public/auth/auth.route.js
@@ -1,6 +1,8 @@
 import express from "express";
 import AuthController from "./auth.controller.js";
 import passport from "passport";
+import { asyncHandler } from "../../../shared/utils/asyncHandler.js";
+
 
 const router = express.Router();
 const authController = new AuthController();
@@ -9,9 +11,9 @@ router.get("/google", passport.authenticate('google',
     { scope: ['profile', 'email'], prompt: "select_account" }
 ));
 
-router.get('/google/callback', passport.authenticate('google', { session: false }),
+router.get('/google/callback', passport.authenticate('google', { session: false }),asyncHandler(
     authController.GoogleCallback.bind(authController)
-);
+));
 
 
 export default router;
