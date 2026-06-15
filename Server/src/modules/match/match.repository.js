fccb4297a3ae312bb0986a95aea414ef483fc365
Swarup Das <swarupdas2046@gmail.com>
2026-06-15 20:37:48 +0530

    some folder deleted

--- Server/src/modules/match/match.repository.js
+++
@@ -1,7 +0,0 @@
-import MatchModel from "./match.model.js";
-
-export default class MatchRepository {
-  async existsBySeries(seriesId) {
-    return await MatchModel.exists({ series: seriesId, isDeleted: false });
-  }
-}
